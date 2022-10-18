a8be6e20ca3db2124c2c462ce7f3161943080036
Rachael Cole <jcrachael@gmail.com>
2022-10-18 18:09:03 +1100

    lesson 12 completed

--- 12_findTheOldest/README.md
+++ 12_findTheOldest/README.md
@@ -6,3 +6,14 @@ Given an array of objects representing people with a birth and death year, retur
 - You should return the whole person object, but the tests mostly just check to make sure the name is correct.
 - this can be done with a couple of chained array methods, or by using `reduce`.
 - One of the tests checks for people with no death-date.. use JavaScript's Date function to get their age as of today.
+
+### Pseudocode
+1. A function takes an array of objects as its parameter
+2. For each object in the array:
+3. Check for year of death
+4. If no year of death:
+5. Subtract year of birth from today's date and return result (age)
+6. If year of death is true:
+7. Subtract year of birth from year of death and return result (age)
+8. Compare each object's age
+9. Return name of object with highest age
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(arrayOfPeople) {
+    // loop through the array of objects
+    for (let i = 0; i < arrayOfPeople.length; i++) {
+        let currentPerson = arrayOfPeople[i];
+        // if no year of death, subtract the person's birth year from today's date
+        if (Object.hasOwn(currentPerson, 'yearOfDeath') === false) {
+            let date = new Date();
+            let year = date.getFullYear();
+            let age = year - currentPerson['yearOfBirth'];
+            currentPerson.age = age;
+        // otherwise, subtract the person's birth year from their death year
+        } else {
+            let deathYear = currentPerson['yearOfDeath']; 
+            let birthYear = currentPerson['yearOfBirth'];
+            let age = deathYear - birthYear;
+            currentPerson.age = age;
+        };                     
+    };
+    // sort by age in descending order and return index 0
+    let newArray = arrayOfPeople.sort((a, b) => {return a.age - b.age;});
+        let sortedArray = newArray.reverse();
+        return sortedArray[0];
+}; 
+    
 // Do not edit below this line
 module.exports = findTheOldest;
